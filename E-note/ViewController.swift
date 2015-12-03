7a68935be717ed0ce1db573241e705855561e33d
Avantech <avantech@pengs-imac.local>
2015-12-03 07:37:28 -0700

    Initial Commit

---
+++ E-note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  E-note
+//
+//  Created by Avantech on 2015-12-03.
+//  Copyright © 2015 Avantech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ E-noteTests/E_noteTests.swift
@@ -0,0 +1,36 @@
+//
+//  E_noteTests.swift
+//  E-noteTests
+//
+//  Created by Avantech on 2015-12-03.
+//  Copyright © 2015 Avantech. All rights reserved.
+//
+
+import XCTest
+@testable import E_note
+
+class E_noteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
